427bafeae80a8940d23e21f9779ee5abca1fbfdc
meriam-business <meddebmeriam9@gmail.com>
2026-05-01 03:24:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -52,9 +52,10 @@ st.markdown("""
     </style>
     
     <p class="main-title">THE VALUE INVESTING GUIDE</p>
-    <p class="brand-name">BY MERIAM.BUSINESS                                                                                                                                                     </p>
+    <p class="brand-name">BY MERIAM.BUSINESS</p>
     """, unsafe_allow_html=True)
-
+st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")
+st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")st.write("  ")
 def meriam_value_investing_report():
     # 1. UNIVERSAL INPUT
     target_ticker = st.text_input("Enter the Stock Ticker (e.g AAPL, MSFT etc ): ").upper()
